c67c3ba51332bac2148d9ce49a4f8313688e29e0
walker-filler <vincent.owusu@dci-student.org>
2024-07-03 21:10:14 +0200

    Add .gitignore file

--- Estate/settings.py
+++ Estate/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = "static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+     "/home/dci-student/STECOYVIN_01/static",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
